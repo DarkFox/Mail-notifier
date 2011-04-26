acaa920679c94f76beee7575908b00a817079aa0
Martin Troels Eberhardt <martin@darkfox.dk>
2011-04-26 10:33:52 +0200

    Use strftime for timestamp

--- mail.py
+++ mail.py
@@ -21,7 +21,7 @@ while True:
   time.sleep(4)
   output = ser.readline()
   if output == '1':
-    timestamp = str(datetime.datetime.now()) + '\n'
+    timestamp = strftime("%Y-%m-%d %H:%M:%S") + '\n'
     f = open(log, 'a')
     f.write(timestamp)
     f.close()
